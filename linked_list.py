53aaf1a6868d7ec5668aef9fb709b8b704d539d2
Hung Nguyen <hungnt89@gmail.com>
2020-06-08 22:20:49 +0700

    Added len(), repr

--- linked_list.py
+++ linked_list.py
@@ -1,10 +1,10 @@
 class Node:
-    def __init__(self, data, nextNode=None):
+    def __init__(self, data):
         self.data = data
-        self.next = nextNode
+        self.next = None
 
     def __repr__(self):
-        return f"Node({self.data!r})"
+        return f"{self.__class__.__name__}({self.data!r})"
 
     def __eq__(self, other):
         return self.data == other.data
@@ -12,21 +12,21 @@ class Node:
 
 class LinkedList:
     def __init__(self, node=None):
-        if node is None:
-            self.head = None
-            self.tail = None
-        else:
-            self.head = node
-            self.tail = node
+        self.head = node
+        self.tail = node
 
     def add_node(self, node):
         if self.head is None:
             self.head = node
+            self.tail = node
         else:
             self.tail.next = node
             self.tail = node
 
     def remove_node(self, node):
+        """
+        Remove the first occurence of the node in the list. Return the node.
+        """
         if self.head == node:
             self.head = self.head.next
         else:
@@ -34,10 +34,30 @@ class LinkedList:
             while currentNode.next != node:
                 currentNode = currentNode.next
             currentNode.next = currentNode.next.next
+        return node
+
+    def __len__(self):
+        """Return linked list's length"""
+        if self.head is None:
+            return 0
+        else:
+            count = 1
+            curNode = self.head
+            while curNode.next is not None:
+                count += 1
+                curNode = curNode.next
+            return count
 
     def __repr__(self):
-        display = "" if self.head is None else str(self.head)
-        return f"LinkedList(head={display})"
+        if self.head is None:
+            return f"{self.__class__.__name__}()"
+        else:
+            string = str(self.head)
+            curNode = self.head
+            while curNode.next is not None:
+                string = string + f", {str(curNode.next)}"
+                curNode = curNode.next
+            return f"{self.__class__.__name__}({string})"
 
 
 if __name__ == '__main__':
@@ -45,6 +65,6 @@ if __name__ == '__main__':
     node2 = Node('26')
     ll1 = LinkedList()
     ll2 = LinkedList(node1)
-    ll3 = LinkedList(node2)
-    for n in ('Pete', True, [2, '3'], {'w': 12, 'g': 'dd'}):
+    ll3 = LinkedList()
+    for n in (12, 'Pete', True, [2, '3'], {'w': 12, 'g': 'dd'}):
         ll3.add_node(Node(n))
